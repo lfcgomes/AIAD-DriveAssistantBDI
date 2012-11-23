590ca8a16af70d1474c274197d10b7f4646b2db8
Luís Dias <lcdias127@gmail.com>
2012-11-23 10:26:24 +0000

    A andar na estrada

--- src/GoPlanEnv.java
+++ src/GoPlanEnv.java
@@ -7,7 +7,11 @@ import jadex.extension.envsupport.math.IVector1;
 import jadex.extension.envsupport.math.IVector2;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import algorithms.PathFinder;
+import algorithms.PathFinder.Node;
 
 /**
  *  Go to a specified position.
@@ -20,23 +24,60 @@ public class GoPlanEnv extends Plan
 	public void body()
 	{
 		Grid2D env = (Grid2D)getBeliefbase().getBelief("env").getFact();
-		
+
 		//Get the position of the final destination, specified in the XML
 		ISpaceObject fd = env.getSpaceObjectsByType("finaldestination")[0];
-		
+		ISpaceObject hm = env.getSpaceObjectsByType("homebase")[0];
+
 		IVector2 target = (IVector2)fd.getProperty("position");
-		
+
+		IVector2 aux = (IVector2)hm.getProperty("position");
+
 		ISpaceObject myself = (ISpaceObject)getBeliefbase().getBelief("myself").getFact();
+
+		myself.setProperty("position", aux);
+
+		System.out.println("MINHA POSICAO "+myself.getProperty("position"));;
+		System.out.println("TARGET "+target.toString());
+
+
+
+		//Calculate de the new direction
+		String dir = null;
 		
+
+
+			
+
 		while(!target.equals(myself.getProperty(Space2D.PROPERTY_POSITION)))
 		{
-			//Calculate de the new direction
-			String dir = null;
+
 			IVector2 mypos = (IVector2)myself.getProperty(Space2D.PROPERTY_POSITION);
 			
-			IVector1 md = env.getShortestDirection(mypos.getX(), target.getX(), true);
+			PathFinder pf = new PathFinder(Utils.map);
+			List<Node> nodes = pf.compute(new PathFinder.Node(((IVector2) myself.getProperty(Space2D.PROPERTY_POSITION)).getXAsInteger(),
+					((IVector2) myself.getProperty(Space2D.PROPERTY_POSITION)).getYAsInteger()),
+					new PathFinder.Node(target.getXAsInteger(),
+							target.getYAsInteger()));
 			
-			switch(md.getAsInteger()){
+			System.out.println("\nPOSICAO "+mypos+ " NODE:" + nodes.get(2));
+			Node next = nodes.get(2);
+			
+			//IVector1 md = env.getShortestDirection(mypos.getX(), target.getX(), true);
+			int md= 0;
+			if(mypos.getYAsInteger() == next.y){
+				if(mypos.getXAsInteger() < next.x)
+					md = 1;
+			}
+			else
+				md = 0;
+					
+			System.out.println("DIRECCAO" +md);
+
+
+
+			System.out.println("NODES "+nodes);
+			/*switch(md.getAsInteger()){
 			case 1:
 				dir = GoAction.RIGHT;
 				break;
@@ -53,8 +94,28 @@ public class GoPlanEnv extends Plan
 					dir = GoAction.UP;
 					break;
 				}
-			}
+			}*/
 			
+			switch(md){
+			case 1:
+				dir = GoAction.RIGHT;
+				break;
+			case -1:
+				dir = GoAction.LEFT;
+				break;
+			default:
+				if(mypos.getXAsInteger() == next.x){
+					if(mypos.getYAsInteger() < next.y)
+						md = 1;
+				}
+				switch(md){
+				case 1:
+					dir = GoAction.DOWN;
+					break;
+
+				}
+			}
+
 			//Inform what is the new direction and execute space action
 			Map params = new HashMap();
 			params.put(GoAction.DIRECTION, dir);
---
+++ src/MoveToLocationPlan.java
@@ -0,0 +1,73 @@
+
+
+import jadex.bdi.planlib.PlanFinishedTaskCondition;
+import jadex.bdi.runtime.Plan;
+import jadex.extension.envsupport.environment.AbstractTask;
+import jadex.extension.envsupport.environment.IEnvironmentSpace;
+import jadex.extension.envsupport.environment.ISpaceObject;
+import jadex.extension.envsupport.environment.space2d.Space2D;
+import jadex.extension.envsupport.math.IVector2;
+import jadex.extension.envsupport.math.Vector1Double;
+import jadex.extension.envsupport.math.Vector2Int;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import algorithms.PathFinder;
+import algorithms.PathFinder.Node;
+
+import java.util.Iterator;
+import java.util.Set;
+
+
+public class MoveToLocationPlan extends Plan {
+
+   
+    public void body() {
+
+        ISpaceObject myself = (ISpaceObject) getBeliefbase().getBelief("myself").getFact();
+        IVector2 dest = (IVector2) getParameter("destination").getValue();
+        getBeliefbase().getBelief("poi").setFact(getParameter("destination").getValue());
+
+        /*Map props = new HashMap();
+        props.put(MoveTask.PROPERTY_DESTINATION, dest);
+        props.put(MoveTask.PROPERTY_SCOPE, getScope().getExternalAccess());
+        props.put(AbstractTask.PROPERTY_CONDITION, new PlanFinishedTaskCondition(getPlanElement()));
+        IEnvironmentSpace space = (IEnvironmentSpace) getBeliefbase().getBelief("environment").getFact();
+
+        PathFinder pf = new PathFinder(Utils.map);
+
+        List<Node> nodes = pf.compute(new PathFinder.Node(((IVector2) myself.getProperty(Space2D.PROPERTY_POSITION)).getXAsInteger(),
+                ((IVector2) myself.getProperty(Space2D.PROPERTY_POSITION)).getYAsInteger()),
+                new PathFinder.Node(dest.getXAsInteger(),
+                dest.getYAsInteger()));
+
+        if (nodes == null) {
+            
+            return;
+        } else {
+            
+            nodes.remove(0);
+            props.put("path", nodes);
+        }
+
+        boolean fim = true;
+        final Set objects = ((Space2D) space).getNearObjects(new Vector2Int(nodes.get(nodes.size() - 1).x, nodes.get(nodes.size() - 1).y), new Vector1Double(1.0));
+        for (Iterator it = objects.iterator(); it.hasNext();) {
+            final ISpaceObject so = (ISpaceObject) it.next();
+            if (so.getType().equals("pointofinterest")) {
+                Utils.dialog.changeText("Vou para: " + so.getProperty("type") + ".");
+                fim = false;
+                break;
+            }
+        }
+
+        if (fim) {
+            Utils.dialog.changeText("Vou para o ponto final.");
+        }
+        Utils.map2 = Utils.map.clone();
+        Object taskid = space.createObjectTask(MoveTask.PROPERTY_TYPENAME, props, myself.getId());
+        SyncResultListener res = new SyncResultListener();
+        space.addTaskListener(taskid, myself.getId(), res);
+        res.waitForResult();*/
+    }
+}
